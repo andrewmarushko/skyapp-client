aa00433949f17ee869dbdd5bea9f77661883367f
Andrew Marushko <andrewmarushko@gmail.com>
2023-03-29 15:39:50 +0200

    CU-862jh86d7 - Setup all needed stuff for starting point

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,16 @@
+'use client'
+import Button from '@geist-ui/core/esm/button'
+import { Button as CustomButton } from '@/components/ui/button'
+
 export default function Home() {
   return (
     <div>
-      <h1>Hello SkyApp</h1>
+      <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl">
+        Taxing Laughter: The Joke Tax Chronicles
+      </h1>
+      <Button>Button from geist</Button>
+      {/* INFO: Just for showcase delete when it's not needed */}
+      <CustomButton>Custom Button</CustomButton>
     </div>
   )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,11 @@ import { fontFamily } from 'tailwindcss/defaultTheme'
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   darkMode: ['class', '[data-theme="dark"]'],
-  content: ['app/**/*.{ts,tsx}', 'components/**/*.{ts,tsx}'],
+  content: [
+    './app/**/*.{js,ts,jsx,tsx}',
+    './pages/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}',
+  ],
   theme: {
     extend: {
       fontFamily: {
