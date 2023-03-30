6d6e79592f392e4359b14bde1daca007ca9a439b
Andrew Marushko <andrewmarushko@gmail.com>
2023-03-31 00:00:01 +0200

    modify next conf

--- next.config.js
+++ next.config.js
@@ -1,8 +1,15 @@
+const path = require("path");
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   experimental: {
     appDir: true,
+    pageEnv: true,
   },
-}
+  webpack(config) {
+    config.resolve.alias["@/app"] = path.join(__dirname, "app");
+    return config;
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
