a7fb84825fd691286cac6e5c9dd878c3364b9d41
Oleksandr Mykhailovskyi <oleksandr_mykhailovskyi@epam.com>
2023-04-03 18:17:06 +0300

    85zrv5dvb - working on typization

--- app/indoor/page.tsx
+++ app/indoor/page.tsx
@@ -1,9 +1,36 @@
+"use client"
+
 import LargeHeading from '@/components/ui/large-heading';
+import useSWR from "swr"
+
+const fetcher = (url) => fetch(url).then((res) => res.json());
 
 const IndoorPage = () => {
+  const { data, error, isLoading } = useSWR(
+    "http://localhost:1337/api/indoors",
+    fetcher
+  );
+
+  if (error) return "An error has occurred.";
+  if (isLoading) return "Loading...";
+  console.log(data, 'data')
   return (
-    <div>
+    <div className=''>
       <LargeHeading size="lg">Indoor main page</LargeHeading>
+      <div className='flex justify-center items-center h-full'>
+        {data.map((data) => (
+          <div key={data.country}>
+            <h3 className='font-extrabold text-2xl'>{data.country}</h3>
+            <div>
+              {data.data.map(({id, name}) => (
+                <p key={id}>
+                  {name}
+                </p>
+              ))}
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
