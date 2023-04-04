dd9e193787573d4e14f27754e8c897a30648f338
Andrew Marushko <andrewmarushko@gmail.com>
2023-04-04 23:51:47 +0200

    Fix navigation

--- components/main-nav.tsx
+++ components/main-nav.tsx
@@ -1,13 +1,14 @@
-"use client"
+'use client';
 
-import * as React from "react"
-import Link from "next/link"
-
-import { siteConfig } from "@/config/site"
-import { cn } from "@/lib/utils"
-import { Icons } from "@/components/icons"
-import { buttonVariants } from "@/components/ui/button"
+import * as React from 'react';
+import Link from 'next/link';
+import * as NavigationMenuRadix from '@radix-ui/react-navigation-menu';
+import { siteConfig } from '@/config/site';
+import { cn } from '@/lib/utils';
+import { Icons } from '@/components/icons';
+import { buttonVariants } from '@/components/ui/button';
 import {
+  ListItem,
   NavigationMenu,
   NavigationMenuContent,
   NavigationMenuIndicator,
@@ -17,93 +18,95 @@ import {
   NavigationMenuTrigger,
   NavigationMenuViewport,
   navigationMenuTriggerStyle,
-} from "@/components/ui/navigation-menu"
-import { Separator } from "@/components/ui/separator"
+} from '@/components/ui/navigation-menu';
+import { Separator } from '@/components/ui/separator';
+
+const components: { title: string, href: string, description: string }[] = [
+  {
+    title: 'Alert Dialog',
+    href: '/docs/primitives/alert-dialog',
+    description:
+      'A modal dialog that interrupts the user with important content and expects a response.',
+  },
+  {
+    title: 'Hover Card',
+    href: '/docs/primitives/hover-card',
+    description:
+      'For sighted users to preview content available behind a link.',
+  },
+  {
+    title: 'Progress',
+    href: '/docs/primitives/progress',
+    description:
+      'Displays an indicator showing the completion progress of a task, typically displayed as a progress bar.',
+  },
+  {
+    title: 'Scroll-area',
+    href: '/docs/primitives/scroll-area',
+    description: 'Visually or semantically separates content.',
+  },
+  {
+    title: 'Tabs',
+    href: '/docs/primitives/tabs',
+    description:
+      'A set of layered sections of content—known as tab panels—that are displayed one at a time.',
+  },
+  {
+    title: 'Tooltip',
+    href: '/docs/primitives/tooltip',
+    description:
+      'A popup that displays information related to an element when the element receives keyboard focus or the mouse hovers over it.',
+  },
+];
 
 export function MainNav() {
   return (
-    <div className="container flex">
-      <Link href="/" className="flex items-center gap-1 cursor-pointer">
-        <Icons.logo className="h-8" />
-        <span className="text-xl font-bold uppercase">
-          {siteConfig.name}
-        </span>
-      </Link>
-      <NavigationMenu>
-        <NavigationMenuList>
-          <NavigationMenuItem>
-            <NavigationMenuTrigger className="h-9">
-              Getting started
-            </NavigationMenuTrigger>
-            <NavigationMenuContent className="">
-              <ul className="one m-0 grid list-none gap-x-[10px] p-[22px] sm:w-[500px] sm:grid-cols-[0.75fr_1fr]">
-                <li className="row-span-3 grid">
-                  <Link href="/" passHref legacyBehavior>
-                    <NavigationMenuLink
-                      className=""
-                    >
-                      <div className="">
-                        {siteConfig.name}
-                      </div>
-                      <p className="">
-                        {siteConfig.description}
-                      </p>
-                    </NavigationMenuLink>
-                  </Link>
-                </li>
-                <ListItem href="/dropzone" title="Dropzone">
-                  Here you can find everything about dropzone for your jumps.
-                </ListItem>
-                <ListItem href="/indoor" title="Indoor Skydive">
-                  How to install dependencies and structure your app.
-                </ListItem>
-                <ListItem href="/" title="Blog">
-                  Cool articles cover your topic ...etc
-                </ListItem>
-              </ul>
-            </NavigationMenuContent>
-          </NavigationMenuItem>
-
-          <NavigationMenuItem className="md:flex">
-            <Link href="/services" legacyBehavior passHref>
-              <NavigationMenuLink
-                className={cn(navigationMenuTriggerStyle(), "h-9")}
-              >
-                Other Services
-              </NavigationMenuLink>
-            </Link>
-          </NavigationMenuItem>
-        </NavigationMenuList>
-        <NavigationMenuIndicator />
-
-        {/* <div className="perspective-[2000px] absolute top-full left-0 flex w-full justify-center"> */}
-        <NavigationMenuViewport className="" />
-      {/* </div> */}
-      </NavigationMenu>
-    </div>
-  )
+    <NavigationMenu>
+      <NavigationMenuList>
+        <NavigationMenuItem>
+          <NavigationMenuTrigger>Getting started</NavigationMenuTrigger>
+          <NavigationMenuContent>
+            <ul className="one m-0 grid list-none gap-x-[10px] p-[6px] sm:w-[500px] sm:grid-cols-[0.75fr_1fr]">
+              <li className="row-span-3 grid">
+                <NavigationMenuLink asChild>
+                  <a
+                    className="flex h-full w-full select-none 
+                      flex-col justify-end rounded-[6px] bg-gradient-to-b from-purple9 to-indigo9 p-[25px] no-underline outline-none focus:shadow-[0_0_0_2px] focus:shadow-violet7"
+                    href="/"
+                  >
+                    <Icons.logo className="h-6 w-6 text-white" />
+                    <div className="mb-2 mt-4 text-lg font-medium text-white">
+                      shadcn/ui
+                    </div>
+                    <p className="text-sm leading-tight text-white/90">
+                      Beautifully designed components built with Radix UI and
+                      Tailwind CSS.
+                    </p>
+                  </a>
+                </NavigationMenuLink>
+              </li>
+              <ListItem href="/docs" title="Introduction">
+                Re-usable components built using Radix UI and Tailwind CSS.
+              </ListItem>
+              <ListItem href="/docs/installation" title="Installation">
+                How to install dependencies and structure your app.
+              </ListItem>
+              <ListItem href="/docs/primitives/typography" title="Typography">
+                Styles for headings, paragraphs, lists...etc
+              </ListItem>
+            </ul>
+          </NavigationMenuContent>
+        </NavigationMenuItem>
+        <NavigationMenuItem>
+          <Link href="/services" legacyBehavior passHref>
+            <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+              Other Services
+            </NavigationMenuLink>
+          </Link>
+        </NavigationMenuItem>
+      </NavigationMenuList>
+      {/* TODO: Need too make proper styles for borders */}
+      {/* <NavigationMenuIndicator /> */}
+    </NavigationMenu>
+  );
 }
-
-const ListItem = React.forwardRef<
-  React.ElementRef<typeof Link>,
-  React.ComponentPropsWithoutRef<typeof Link>
->(({ className, title, children, href, ...props }, ref) => {
-  return (
-    <li>
-      <Link href={href} passHref legacyBehavior {...props}>
-        <NavigationMenuLink
-          className={cn(
-            "focus:shadow-[0_0_0_2px] focus:shadow-violet7 hover:bg-mauve3 block select-none rounded-[6px] p-3 text-[15px] leading-none no-underline outline-none transition-colors",
-            className
-          )}
-        >
-          <div className="text-violet12 mb-[5px] font-medium leading-[1.2]">{title}</div>
-          <p className="text-mauve11 leading-[1.4]">
-            {children}
-          </p>
-        </NavigationMenuLink>
-      </Link>
-    </li>
-  )
-})
-ListItem.displayName = "ListItem"
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,6 @@
 import { fontFamily, colors } from 'tailwindcss/defaultTheme';
 import plugin from 'tailwindcss/plugin';
+const { blackA, mauve, violet, indigo, purple } = require('@radix-ui/colors');
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
@@ -9,8 +10,21 @@ module.exports = {
   content: ['app/**/*.{ts,tsx}', 'components/**/*.{ts,tsx}'],
 
   theme: {
+    container: {
+      center: true,
+      padding: '1.5rem',
+      screens: {
+        '2xl': '1440px',
+      },
+    },
     extend: {
       colors: {
+        ...blackA,
+        ...mauve,
+        ...violet,
+        ...purple,
+        ...indigo,
+
         'accent-1': 'var(--accents-1)',
         'accent-2': 'var(--accents-2)',
         'accent-3': 'var(--accents-3)',
