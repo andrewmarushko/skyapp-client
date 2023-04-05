41f314bb22d9fdb38eb4a0d93d7b5905d25e0b9e
Andrew Marushko <andrewmarushko@gmail.com>
2023-04-05 12:22:56 +0200

    Fix build error

--- app/indoor/page.tsx
+++ app/indoor/page.tsx
@@ -31,20 +31,22 @@ const IndoorPage = async () => {
     <Page>
       <LargeHeading size="lg">Indoor main page</LargeHeading>
       <div className="grid gap-10">
-        {data.map((countryData: any) => (
-          <div key={countryData.country} className="grid grid-cols-4 gap-12">
-            {countryData.data.map((indoor: any) => (
-              <div
-                className="col-span-1 rounded-xl border border-black p-4"
-                key={indoor.id}
-              >
-                <p>{indoor.indoorName}</p>
-                <p>{indoor.indoorLocation.city}</p>
-                <p>{indoor.indoorLocation.address}</p>
-              </div>
-            ))}
-          </div>
-        ))}
+        {data &&
+          data.map((countryData: any) => (
+            <div key={countryData.country} className="grid grid-cols-4 gap-12">
+              {countryData &&
+                countryData.data.map((indoor: any) => (
+                  <div
+                    className="col-span-1 rounded-xl border border-black p-4"
+                    key={indoor.id}
+                  >
+                    <p>{indoor.indoorName}</p>
+                    <p>{indoor.indoorLocation.city}</p>
+                    <p>{indoor.indoorLocation.address}</p>
+                  </div>
+                ))}
+            </div>
+          ))}
       </div>
       <div className="mt-4 flex w-full justify-center">
         <Button>Load More</Button>
