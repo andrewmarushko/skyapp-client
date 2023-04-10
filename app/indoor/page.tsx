d9f5ac96cb3f8e3acfa648d4650155cf1e76f202
Andrew Marushko <andrewmarushko@gmail.com>
2023-04-11 00:44:53 +0200

    delete unused seo stuff for now

--- app/indoor/page.tsx
+++ app/indoor/page.tsx
@@ -8,34 +8,9 @@ export async function generateMetadata(): Promise<Metadata> {
   const { seo } = await getIndoorPageData();
 
   return {
-    metadataBase: new URL(
-      `https://${seo.canonicalURL}/indoor` ||
-        `https://${process.env.NEXT_PUBLIC_DEV_URL}/indoor`,
-    ),
+    metadataBase: new URL(`${seo.canonicalURL}`),
     title: seo.metaTitle,
     description: seo.metaDescription,
-    robots: {
-      index: false,
-      follow: true,
-      nocache: true,
-      googleBot: {
-        index: true,
-        follow: false,
-        noimageindex: true,
-        "max-video-preview": -1,
-        "max-image-preview": "large",
-        "max-snippet": -1,
-      },
-    },
-    twitter: {
-      card: "summary_large_image",
-      title: "Next.js",
-      description: "The React Framework for the Web",
-      siteId: "1467726470533754880",
-      creator: "@nextjs",
-      creatorId: "1467726470533754880",
-      images: ["https://nextjs.org/og.png"],
-    },
     viewport: {
       width: "device-width",
       initialScale: 1,
@@ -66,7 +41,7 @@ const IndoorPage = async () => {
           ))}
       </div>
       <div className="mt-4 flex w-full justify-center">
-        <Button>Load More</Button>
+        <Button onClick={() => console.log('some data')}>Load More</Button>
       </div>
     </Page>
   );
