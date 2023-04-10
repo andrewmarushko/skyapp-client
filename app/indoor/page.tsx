57f52e0ce8eef72fb07149312184581ab2e6438e
Andrew Marushko <andrewmarushko@gmail.com>
2023-04-10 02:42:02 +0200

    add indoor-page api

--- app/indoor/page.tsx
+++ app/indoor/page.tsx
@@ -1,21 +1,51 @@
-import { getIndoorsData } from '@/api-service/indoor-api';
-import { Icons } from '@/components/icons';
-import { Button } from '@/components/ui/button';
-import LargeHeading from '@/components/ui/large-heading';
+import {
+  getAllIndoors,
+  getIndoorPageData,
+  getIndoorsData,
+} from "@/api-service/indoor-api";
+import { Icons } from "@/components/icons";
+import { Button } from "@/components/ui/button";
+import LargeHeading from "@/components/ui/large-heading";
 
-import Page from '@/components/ui/page';
-import { IndoorDataListInterface, IndoorDataItemInterface } from '@/types/nav';
-import Image from 'next/image';
-import Link from 'next/link';
+import Page from "@/components/ui/page";
+import { IndoorDataItemInterface, IndoorDataListInterface } from "@/types/nav";
+import { Metadata } from "next";
+import Image from "next/image";
+import Link from "next/link";
+
+export async function generateMetadata(): Promise<Metadata> {
+  const pageData = await fetch(
+    `${process.env.NEXT_PUBLIC_DEV_URL}/indoor-page`,
+  );
+  const { seo } = await pageData.json();
+
+  return {
+    metadataBase: new URL(
+      `https://${seo.canonicalURL}/indoor` ||
+        `https://${process.env.NEXT_PUBLIC_DEV_URL}/indoor`,
+    ),
+    title: seo.metaTitle,
+    description: seo.metaDescription,
+  };
+}
 
 const IndoorPage = async () => {
-  const data = await getIndoorsData();
+  const [windTunnels, pageIndoorData] = await Promise.all([
+    getAllIndoors(),
+    getIndoorPageData(),
+  ]);
+
+  console.log("windtunnels", windTunnels);
+
   return (
     <Page>
-      <LargeHeading size="lg">Indoor main page</LargeHeading>
+      <div>
+        <LargeHeading size="lg">{pageIndoorData.hero.title}</LargeHeading>
+        <h2>{pageIndoorData.hero.subtitle}</h2>
+      </div>
       <div className="grid gap-10">
-        {data &&
-          data.map((countryData: IndoorDataListInterface) => (
+        {windTunnels &&
+          windTunnels.map((countryData: IndoorDataListInterface) => (
             <div key={countryData.country} className="grid grid-cols-3 gap-8">
               {countryData &&
                 countryData.data.map(
@@ -33,54 +63,54 @@ const IndoorPage = async () => {
                         <Image
                           src={coverImage?.url}
                           alt={coverImage?.alternativeText}
-                          width={coverImage?.width}
+                          width={200}
                           height={200}
                           className="h-[200px] w-full rounded-t-xl object-cover"
                           priority
                           quality={100}
                         />
-                      </Link>
 
-                      <div className="rounded-b-xl border border-t-0 border-[#d2d2d2] p-3">
-                        <div className="flex justify-between">
-                          <p className="mb-3 font-semibold underline">
-                            <Link
-                              href={`/indoor/${indoorLocation.country}/${indoorLocation.city}/${id}`}
-                            >
-                              {title}
-                            </Link>
-                          </p>
-                          {websiteUrl && (
-                            <a target="_blank" href={websiteUrl}>
-                              <div className="flex items-center gap-1">
-                                <p>Visit Site</p>
-                                <Icons.externalLink
-                                  width="20px"
-                                  height="20px"
-                                />
-                              </div>
-                            </a>
-                          )}
+                        <div className="rounded-b-xl border border-t-0 border-[#d2d2d2] p-3">
+                          <div className="flex justify-between">
+                            <p className="mb-3 font-semibold underline">
+                              <Link
+                                href={`/indoor/${indoorLocation.country}/${indoorLocation.city}/${id}`}
+                              >
+                                {title}
+                              </Link>
+                            </p>
+                            {websiteUrl && (
+                              <a target="_blank" href={websiteUrl}>
+                                <div className="flex items-center gap-1">
+                                  <p>Visit Site</p>
+                                  <Icons.externalLink
+                                    width="20px"
+                                    height="20px"
+                                  />
+                                </div>
+                              </a>
+                            )}
+                          </div>
+                          <div>
+                            <span>
+                              <Link
+                                className="underline"
+                                href={`/indoor/${indoorLocation.country}`}
+                              >
+                                {indoorLocation.country + ", "}
+                              </Link>
+                            </span>
+                            <span>
+                              <Link
+                                className="underline"
+                                href={`/indoor/${indoorLocation.country}/${indoorLocation.city}`}
+                              >
+                                {indoorLocation.city}
+                              </Link>
+                            </span>
+                          </div>
                         </div>
-                        <div>
-                          <span>
-                            <Link
-                              className="underline"
-                              href={`/indoor/${indoorLocation.country}`}
-                            >
-                              {indoorLocation.country + ', '}
-                            </Link>
-                          </span>
-                          <span>
-                            <Link
-                              className="underline"
-                              href={`/indoor/${indoorLocation.country}/${indoorLocation.city}`}
-                            >
-                              {indoorLocation.city}
-                            </Link>
-                          </span>
-                        </div>
-                      </div>
+                      </Link>
                     </div>
                   ),
                 )}
