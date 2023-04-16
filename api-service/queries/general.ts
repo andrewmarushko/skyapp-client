ec54f6b9cbb933235cc65c56999a1744668c24ec
Andrew Marushko <andrewmarushko@andrews-imac.home>
2023-04-16 03:40:19 +0200

    Configure data layer

---
+++ api-service/queries/general.ts
@@ -0,0 +1,32 @@
+import qs from 'qs'
+
+export const LOGO_QUERY = qs.stringify({
+    populate: [
+    'logo',
+    'logo.logo',
+    ],
+},
+{
+    encodeValuesOnly: true,
+});
+
+export const NAVIGATION_QUERY = qs.stringify({
+    populate: [
+        'mainNavigation',
+        'mainNavigation.navigationLinks,mainNavigation.panel',
+        'mainNavigation.panel.push,mainNavigation.panel.links',
+        'mainNavigation.panel.push.link,mainNavigation.panel.links.link,mainNavigation.panel.push.cover'
+    ],
+},{
+    encodeValuesOnly: true,
+})
+
+export const FOOTER_QUERY = qs.stringify({
+    populate: [
+        'footer',
+        'footer.links,footer.copyright,footer.subscribe,footer.social',
+        'footer.navigation.links,footer.subscribe.submitButton,footer.social.link'
+    ],
+},{
+    encodeValuesOnly: true,
+})
