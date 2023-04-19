1c0e0e87969db92ffa2f8774c4ecda4e3a72f913
Andrew Marushko <andrewmarushko@gmail.com>
2023-04-19 19:25:19 +0200

    Update latest (#49)
    * CU-862jk516x - Card Component. The task is completed
    
    * CU-862jk516x - Card Component. Renamed tailwind custom classes
    
    * Update tailwind.config.js
    
    Change naming as Tailwind propose
    
    * CU-862jk516x - Card Component. Fixed the error in tailwind.config
    
    * CU-85zrwkga0 - Fix. Improve Tailwind config. Created the config for colors
    
    * CU-85zrwkga0 - Fix. Improve Tailwind config. Fixed colors settings
    
    * CU-85zrwkga0 - Fix. Improve Tailwind config. Added assest default and 9 values
    
    * CU-862jk516x - Card component. Fixed the styles for the Card component
    
    * CU-862jk516x - Card component. Fixed the styles for the Card component. Refactoring
    
    * Add basic layout for Content (not styled)
    
    * fix build error and add baseUrl into tsconfig
    
    * Add request wrapper for default fetch
    
    * Fix layout major problem
    
    * fix build issue with missing keys
    
    * Manage header data from API's and add footer data also (#37)
    
    * Manage header data from API's and add footer data also
    
    * fix build and lint error
    
    ---------
    
    Co-authored-by: Andrew Marushko <andrewmarushko@andrews-imac.home>
    
    * Update README.md
    
    * Update README.md
    
    * fix request url
    
    * Data driven  cu 862jmmykx (#39)
    
    * Manage header data from API's and add footer data also
    
    * fix build and lint error
    
    ---------
    
    Co-authored-by: Andrew Marushko <andrewmarushko@andrews-imac.home>
    
    * Data driven  cu 862jmmykx (#40)
    
    * Manage header data from API's and add footer data also
    
    * fix build and lint error
    
    * Configure data layer
    
    ---------
    
    Co-authored-by: Andrew Marushko <andrewmarushko@andrews-imac.home>
    
    * Data driven  cu 862jmmykx (#41)
    
    * Manage header data from API's and add footer data also
    
    * fix build and lint error
    
    * Configure data layer
    
    * Upadate packages and install qs types
    
    ---------
    
    Co-authored-by: Andrew Marushko <andrewmarushko@andrews-imac.home>
    
    * CU-862jmeheh - Button (#38) (#42)
    
    * Add basic layout for Content (not styled)
    
    * fix build error and add baseUrl into tsconfig
    
    * Add request wrapper for default fetch
    
    * Fix layout major problem
    
    * fix build issue with missing keys
    
    * CU-862jmeheh - Button
    
    * Manage header data from API's and add footer data also (#37)
    
    * Manage header data from API's and add footer data also
    
    * fix build and lint error
    
    ---------
    
    
    
    * CU-862jmeheh - Button. Fixed build issue
    
    * Update README.md
    
    * Update README.md
    
    * fix request url
    
    * Data driven  cu 862jmmykx (#39)
    
    * Manage header data from API's and add footer data also
    
    * fix build and lint error
    
    ---------
    
    
    
    * Data driven  cu 862jmmykx (#40)
    
    * Manage header data from API's and add footer data also
    
    * fix build and lint error
    
    * Configure data layer
    
    ---------
    
    
    
    * Data driven  cu 862jmmykx (#41)
    
    * Manage header data from API's and add footer data also
    
    * fix build and lint error
    
    * Configure data layer
    
    * Upadate packages and install qs types
    
    ---------
    
    
    
    * change naming of styles variables and add call to action variant
    
    ---------
    
    Co-authored-by: OleksandrMykhailovskyi <39292493+OleksandrMykhailovskyi@users.noreply.github.com>
    Co-authored-by: Oleksandr Mykhailovskyi <oleksandr_mykhailovskyi@epam.com>
    Co-authored-by: Andrew Marushko <andrewmarushko@andrews-imac.home>
    
    * Add missing page and files (#43)
    
    * Seo integration for all pages cu 862jmzrg1 (#48)
    
    * add seo request
    
    * add seo for all pages
    
    ---------
    
    Co-authored-by: Andrew Marushko <andrewmarushko@andrews-imac.home>
    
    ---------
    
    Co-authored-by: Oleksandr Mykhailovskyi <oleksandr_mykhailovskyi@epam.com>
    Co-authored-by: Andrew Marushko <andrewmarushko@andrews-imac.home>
    Co-authored-by: OleksandrMykhailovskyi <39292493+OleksandrMykhailovskyi@users.noreply.github.com>

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -1,6 +1,43 @@
+import { getPageSeo } from '@/api-service/seo';
 import LargeHeading from '@/components/ui/large-heading';
 import Page from '@/components/ui/page';
 import Paragraph from '@/components/ui/paragraph';
+
+import { Metadata } from 'next';
+
+const defaultSeo = {
+
+  title: "Blog",
+  description: "Blog page"
+}
+
+export async function generateMetadata(): Promise<Metadata> {
+  const {seo} = await getPageSeo('blog-page')
+
+  if (!seo) return defaultSeo
+
+  return {
+    metadataBase: new URL(`${seo.metadataBase}`),
+    title: seo.metaTitle,
+    description: seo.metaDescription,
+    applicationName: seo.applicationName,
+    keywords: seo.keywords,
+    formatDetection: {
+      email: seo.format_description.email,
+      telephone: seo.format_description.telephone,
+      address: seo.format_description.address
+    },
+    viewport: {
+      width: seo.viewport.width,
+      initialScale: seo.viewport.initial_scale,
+    },
+    robots: {
+      index: seo.robots.index,
+      follow: seo.robots.follow,
+      nocache: seo.robots.nocache,
+    }
+  }
+}
 
 
 const BlogPage = () => {
