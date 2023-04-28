b95db3b532261588a662238515f06eb73bc1a61a
OleksandrMykhailovskyi <39292493+OleksandrMykhailovskyi@users.noreply.github.com>
2023-04-28 17:34:52 +0300

    CU-862jq0vt4 - Search. Design part is ready (#58)
    Co-authored-by: Oleksandr Mykhailovskyi <oleksandr_mykhailovskyi@epam.com>

---
+++ components/search.tsx
@@ -0,0 +1,18 @@
+import { FunctionComponent } from 'react';
+import { Icons } from '@/components/icons';
+import { Input } from '@/components/ui/input';
+
+interface SearchInterface {
+}
+
+export const Search: FunctionComponent<SearchInterface> = () => {
+  return (
+    <div className='bg-sk-light dark:bg-sk-dark w-full h-10 flex items-center max-w-full text-sm border rounded border-accent-800 dark:border-accent-500 focus-within:border-accent-400 dark:focus-within:border-accent-800 transition-colors'>
+      <span className='relative h-full text-accent-500 flex items-center pl-3'>
+        <Icons.search className='h-5 w-5' />
+      </span>
+      {/* TODO: Add Reset input value button from Vercel */}
+      <Input placeholder='Search...' variant={'search'} fullWidth type="search"/>
+    </div>
+  );
+};
