f6e89799a58e9f3858b58fbed52ed5fd9e80f73e
OleksandrMykhailovskyi <39292493+OleksandrMykhailovskyi@users.noreply.github.com>
2023-04-29 02:04:49 +0300

    CU-862jq0vp0 - Show tubes grid. Added the styles for input card (#60)

--- components/ui/card.tsx
+++ components/ui/card.tsx
@@ -1,19 +1,45 @@
-import { FunctionComponent, ReactNode } from 'react';
+import { cn } from '@/lib/utils';
+import { VariantProps, cva } from 'class-variance-authority';
+import { forwardRef } from 'react';
 
-interface CardPropsInterface {
-  children?: ReactNode;
-  className?: string;
-}
+const cardVariants = cva(
+  "",
+  {
+    variants: {
+      variant: {
+        default: 
+          "pointer ease flex h-full w-full max-w-xs flex-col overflow-hidden rounded bg-sk-light text-inherit no-underline shadow-geist-shadow-sm transition-box-shadow duration-200 focus-within:shadow-geist-border-shadow-white hover:shadow-geist-border-shadow-white focus:shadow-geist-border-shadow-white dark:bg-sk-dark dark:shadow-geist-shadow-sm-dark dark:focus-within:shadow-geist-border-shadow-white-dark dark:hover:shadow-geist-border-shadow-white-dark dark:focus:shadow-geist-border-shadow-white-dark", 
+      },
+      fullWidth: {
+        true: 'w-full' 
+      },
+      size: {
+        default: "",
+      },
+    },
+    defaultVariants: {
+      variant: "default",
+      size: "default",
+    },
+  }
+)
 
-// TODO: Use cva for styles
+interface CardPropsInterface
+  extends React.HTMLAttributes<HTMLDivElement>,
+    VariantProps<typeof cardVariants> {}
 
-export const Card: FunctionComponent<CardPropsInterface> = ({
-  children,
-  className,
-}) => {
+const Card = forwardRef<HTMLDivElement, CardPropsInterface>(({ className, variant, size, fullWidth, children, ...props }, ref) => {
   return (
-    <div className="pointer ease flex h-full w-full max-w-xs flex-col overflow-hidden rounded bg-sk-light text-inherit no-underline shadow-geist-shadow-sm transition-box-shadow duration-200 focus-within:shadow-geist-border-shadow-white hover:shadow-geist-border-shadow-white focus:shadow-geist-border-shadow-white dark:bg-sk-dark dark:shadow-geist-shadow-sm-dark dark:focus-within:shadow-geist-border-shadow-white-dark dark:hover:shadow-geist-border-shadow-white-dark dark:focus:shadow-geist-border-shadow-white-dark">
+    <div 
+      className={cn(cardVariants({ variant, size, fullWidth, className }))}
+      ref={ref}
+      {...props}
+    >
       {children}
     </div>
-  );
-};
+  )
+})
+
+Card.displayName = "Card"
+
+export { Card, cardVariants }
