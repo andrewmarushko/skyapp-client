5503b290840b1b32419cee303eb2a7fda6e03969
Oleksandr Mykhailovskyi <oleksandr_mykhailovskyi@epam.com>
2023-05-03 04:16:50 +0300

    CU-862jqw35r - Tailwind config. Modified global.css and tailwind.config files

--- tailwind.config.js
+++ tailwind.config.js
@@ -19,10 +19,6 @@ module.exports = {
         //Please keep in mind, that this breakpoint has 1150px value on vercel.com site
         lg: '950px',
       },
-      screens: {
-        //Please keep in mind, that this breakpoint has 1150px value on vercel.com site
-        lg: '950px',
-      },
       colors: {
         ...blackA,
         ...mauve,
@@ -147,58 +143,6 @@ module.exports = {
           800: 'var(--geist-experimental-color-gray-a9)',
           900: 'var(--geist-experimental-color-gray-a10)',
         },
-        //experimental gray
-        'experimental-gray': {
-          DEFAULT: 'var(--geist-experimental-color-gray-1)',
-          100: 'var(--geist-experimental-color-gray-2)',
-          200: 'var(--geist-experimental-color-gray-3)',
-          300: 'var(--geist-experimental-color-gray-4)',
-          400: 'var(--geist-experimental-color-gray-5)',
-          500: 'var(--geist-experimental-color-gray-6)',
-          600: 'var(--geist-experimental-color-gray-7)',
-          700: 'var(--geist-experimental-color-gray-8)',
-          800: 'var(--geist-experimental-color-gray-9)',
-          900: 'var(--geist-experimental-color-gray-10)',
-        },
-        //experimental gray dark
-        'experimental-gray-dark': {
-          DEFAULT: 'var(--geist-experimental-color-gray-1-dark)',
-          100: 'var(--geist-experimental-color-gray-2-dark)',
-          200: 'var(--geist-experimental-color-gray-3-dark)',
-          300: 'var(--geist-experimental-color-gray-4-dark)',
-          400: 'var(--geist-experimental-color-gray-5-dark)',
-          500: 'var(--geist-experimental-color-gray-6-dark)',
-          600: 'var(--geist-experimental-color-gray-7-dark)',
-          700: 'var(--geist-experimental-color-gray-8-dark)',
-          800: 'var(--geist-experimental-color-gray-9-dark)',
-          900: 'var(--geist-experimental-color-gray-10-dark)',
-        },
-        //experimental gray a for dark theme
-        'experimental-gray-a-dark': {
-          DEFAULT: 'var(--geist-experimental-color-gray-a1-dark)',
-          100: 'var(--geist-experimental-color-gray-a2-dark)',
-          200: 'var(--geist-experimental-color-gray-a3-dark)',
-          300: 'var(--geist-experimental-color-gray-a4-dark)',
-          400: 'var(--geist-experimental-color-gray-a5-dark)',
-          500: 'var(--geist-experimental-color-gray-a6-dark)',
-          600: 'var(--geist-experimental-color-gray-a7-dark)',
-          700: 'var(--geist-experimental-color-gray-a8-dark)',
-          800: 'var(--geist-experimental-color-gray-a9-dark)',
-          900: 'var(--geist-experimental-color-gray-a10-dark)',
-        },
-        //experimental gray a for light theme
-        'experimental-gray-a': {
-          DEFAULT: 'var(--geist-experimental-color-gray-a1)',
-          100: 'var(--geist-experimental-color-gray-a2)',
-          200: 'var(--geist-experimental-color-gray-a3)',
-          300: 'var(--geist-experimental-color-gray-a4)',
-          400: 'var(--geist-experimental-color-gray-a5)',
-          500: 'var(--geist-experimental-color-gray-a6)',
-          600: 'var(--geist-experimental-color-gray-a7)',
-          700: 'var(--geist-experimental-color-gray-a8)',
-          800: 'var(--geist-experimental-color-gray-a9)',
-          900: 'var(--geist-experimental-color-gray-a10)',
-        },
         //button custom colors
         button: {
           default: {
@@ -215,134 +159,87 @@ module.exports = {
         },
         'geist-foreground': 'var(--geist-foreground)'
       },
+      // TODO: remove these spaces from the components and change them to the usual sizes
       spacing: {
-        'geist-gap-half': 'var(--geist-gap-half)',
-        'geist-gap': 'var(--geist-gap)',
-        'geist-gap-quarter': 'var(--geist-gap-quarter)',
-        '1.5': 'var(--geist-space-3x)',
-        '2.5': 'var(--geist-space-5x)',
-        '0.5': 'var(--geist-space)',
-        125: 'var(--geist-space-250x)',
-        'navigation-pointer': 'var(--space-navigation-pointer)',
-        'geist-gap-half': 'var(--geist-gap-half)',
-        'geist-gap': 'var(--geist-gap)',
-        'geist-gap-quarter': 'var(--geist-gap-quarter)',
-        '1.5': 'var(--geist-space-1-5x)',
-        '2.5': 'var(--geist-space-2-5x)',
-        125: 'var(--geist-space-125x)',
-        'navigation-pointer': 'var(--space-navigation-pointer)',
+        // 'geist-gap-half': 'var(--geist-gap-half)',
+        // 'geist-gap': 'var(--geist-gap)',
+        // 'geist-gap-quarter': 'var(--geist-gap-quarter)',
+        // '1.5': 'var(--geist-space-3x)',
+        // '2.5': 'var(--geist-space-5x)',
+        // '0.5': 'var(--geist-space)',
+        // 125: 'var(--geist-space-250x)',
       },
       borderRadius: {
         DEFAULT: 'var(--geist-radius)',
-        marketing: 'var(--geist-marketing-radius)',
-        link: 'var(--geist-link-radius)',
-        100: 'var(--border-radius-100)',
-        50: 'var(--border-radius-50)',
+        // TODO: modify the classes names in the components
+        '6.25rem': 'var(--border-radius-25x)',
         xs: 'var(--border-radius-xs)',
-        link: 'var(--geist-link-radius)'
+        // 100: 'var(--border-radius-100)',
+        // 50: 'var(--border-radius-50)',
+        // xs: 'var(--border-radius-xs)',
       },
       fontSize: {
-        64: 'var(--font-size-64)',
-        'geist-action-large-font': 'var(--geist-action-large-font)',
-        'geist-action-small-font': 'var(--geist-action-small-font)',
-        'geist-action-font': 'var(--geist-action-font)',
-        'geist-form-font': 'var(--geist-form-font)',
-        'geist-action-font': 'var(--geist-action-font)',
-        'geist-form-font': 'var(--geist-form-font)'
+        '4rem': 'var(--font-size-4rem)',
+        // TODO: remove font word from components
+        'geist-action-large': 'var(--geist-action-large-font)',
+        'geist-action-small': 'var(--geist-action-small-font)',
+        'geist-action': 'var(--geist-action-font)',
+        'geist-form': 'var(--geist-form-font)',
       },
       lineHeight: {
-        '14px': 'var(--line-height-14px)',
-        '74px': 'var(--line-height-74px)',
-        14: 'var(--line-height-14)',
-        'geist-action-large-line-height': 'var(--geist-action-large-line-height)',
-        'geist-action-small-line-height': 'var(--geist-action-small-line-height)',
-        'geist-action-line-height': 'var(--geist-action-line-height)',
-        'geist-form-line-height': 'var(--geist-form-line-height)',
         0: 'var(--line-height-0)',
-        'geist-action-line-height': 'var(--geist-action-line-height)',
-        'geist-form-line-height': 'var(--geist-form-line-height)'
+        // TODO: change 14px and 74px in components
+        3.5: 'var(--line-height-3x-half)',
+        18.5: 'var(--line-height-18x-half)',
+        // '14px': 'var(--line-height-3x-half)',
+        // '74px': 'var(--line-height-18x-half)',
+        14: 'var(--line-height-14)',
+        // TODO: remove line-height word from components
+        'geist-action-large': 'var(--geist-action-large-line-height)',
+        'geist-action-small': 'var(--geist-action-small-line-height)',
+        'geist-action': 'var(--geist-action-line-height)',
+        'geist-form': 'var(--geist-form-line-height)',
       },
       height: {
-        'geist-action-large-height': 'var(--geist-action-large-height)',
-        'geist-action-small-height': 'var(--geist-action-small-height)',
-        'geist-action-height': 'var(--geist-action-height)',
-        'geist-form-height': 'var(--geist-form-height)',
-        '1px': 'var(--header-1px)',
+        // TODO: remove height word from components
+        'geist-action-large': 'var(--geist-action-large-height)',
+        'geist-action-small': 'var(--geist-action-small-height)',
+        'geist-action': 'var(--geist-action-height)',
+        'geist-form': 'var(--geist-form-height)',
+        // TODO: change 1px to px in the components
+        // '1px': 'var(--header-1px)',
         'geist-gap-x2': 'var(--geist-gap-x2)',
-        'geist-gap-x2': 'var(--geist-gap-x2)',
-        'nav-menu-height': 'var(--nav-menu-height)'
       },
       minHeight: {
         'header-height': 'var(--header-height)',
       },
       maxHeight: {
         '60vh': 'var(--height-60vh)',
-      },
-      maxHeight: {
-        '60vh': 'var(--height-60vh)',
         'geist-action-height': 'var(--geist-action-height)',
         'geist-form-height': 'var(--geist-form-height)',
-        '1px': 'var(--header-1px)',
-        'geist-gap-x2': 'var(--geist-gap-x2)',
-        'geist-gap-x2': 'var(--geist-gap-x2)',
-        'nav-menu-height': 'var(--nav-menu-height)'
-      },
-      minHeight: {
-        'header-height': 'var(--header-height)',
-      },
-      maxHeight: {
-        '60vh': 'var(--height-60vh)',
-      },
-      maxHeight: {
-        '60vh': 'var(--height-60vh)',
+        // TODO: change 1px to px in the components
+        // '1px': 'var(--header-1px)',
       },
       letterSpacing: {
         'tight-title':'var(--letter-spacing-title)',
         'tight-subtitle': 'var(--letter-spacing-subtitle)'
       },
       gridTemplateColumns: {
-        '1-3': 'var(--grid-template-columns-1-3)'
+        // TODO change 1-3 to 1-2 in the component
+        // '1-3': 'var(--grid-template-columns-1-2)' 
+        '1-2': 'var(--grid-template-columns-1-2)'
       },
       boxShadow: {
-        'geist-shadow-sm': 'var(--shadow-small)',
-        'geist-shadow-md': 'var(--shadow-medium)',
-        'geist-shadow-sm-dark': 'var(--shadow-small-dark)',
-        'geist-shadow-md-dark': 'var(--shadow-medium-dark)',
-        'geist-border-shadow-white': 'var(--shadow-medium)',
-        'geist-border-shadow-white-dark': 'var(--shadow-medium-border)',
+        // TODO: remove shadow word from components
+        'geist-sm': 'var(--shadow-small)',
+        'geist-md': 'var(--shadow-medium)',
+        'geist-sm-dark': 'var(--shadow-small-dark)',
+        'geist-border-white': 'var(--shadow-medium)',
+        'focus': 'var(--next-focus-shadow)',
+        '3xl': 'var(--shadow-3xl)',
         'header-border-bottom': 'var(--header-border-bottom)',
         'header-border-bottom-dark': 'var(--header-border-bottom-dark)',
-        '3xl': 'var(--shadow-3xl)',
         'sm-gray': 'var(--shadow-sm-gray)',
-        'focus-shadow': 'var(--next-focus-shadow)'
-      },
-      transitionProperty: {
-        color: 'var(--transition-property-color)',
-        'bg-and-color': 'var(--transition-property-color-and-background)'
-      },
-      transitionProperty: {
-        color: 'var(--transition-property-color)',
-        'bg-and-color': 'var(--transition-property-color-and-background)'
-      },
-      transitionProperty: {
-        color: 'var(--transition-property-color)',
-        'bg-and-color': 'var(--transition-property-color-and-background)',
-        'header-border-bottom': 'var(--header-border-bottom)',
-        'header-border-bottom-dark': 'var(--header-border-bottom-dark)',
-        '3xl': 'var(--shadow-3xl)',
-        'sm-gray': 'var(--shadow-sm-gray)'
-      },
-      transitionProperty: {
-        color: 'var(--transition-property-color)',
-        'bg-and-color': 'var(--transition-property-color-and-background)'
-      },
-      transitionProperty: {
-        color: 'var(--transition-property-color)',
-        'bg-and-color': 'var(--transition-property-color-and-background)'
-      },
-      transitionProperty: {
-        color: 'var(--transition-property-color)',
-        'bg-and-color': 'var(--transition-property-color-and-background)'
       },
       transitionTimingFunction: {
         DEFAULT: 'var(--transition-timing-function-default-ease)'
@@ -352,68 +249,36 @@ module.exports = {
         'background-color': 'var(--transition-property-background-color)',
         'colors-shadow-transform': 'var(--transition-property-colors-shadow-transform)',
         'max-height': 'var(--transition-property-max-height)',
-        'max-height': 'var(--transition-property-max-height)',
         'width-height': 'var(--transition-width-height)',
-        'width-transform': 'var(--transition-width-transform)'
+        'width-transform': 'var(--transition-width-transform)',
+        color: 'var(--transition-property-color)',
+        'bg-and-color': 'var(--transition-property-color-and-background)',
       },
       transformOrigin: {
         'top-center': 'var(--transform-origin-top-center)',
       },
-      transitionProperty: {
-        'background-color-and-box-shadow': 'var(--transition-property-background-color-and-box-shadow)',
-        'background-color': 'var(--transition-property-background-color)',
-        'colors-shadow-transform': 'var(--transition-property-colors-shadow-transform)',
-        'max-height': 'var(--transition-property-max-height)',
-        'max-height': 'var(--transition-property-max-height)',
-        'width-height': 'var(--transition-width-height)',
-        'width-transform': 'var(--transition-width-transform)'
-      },
-      transformOrigin: {
-        'top-center': 'var(--transform-origin-top-center)',
-      },
+      // TODO change minWidth and maxWidth 164 to custom-10.25rem in the component
       minWidth: {
-        164: "var(--min-width-164)"
+        'custom-10.25rem': "var(--min-width-custom-41x)"
       },
       maxWidth: {
-        164: "var(--max-width-164)",
-        240: "var(--max-width-240)",
+        'custom-10.25rem': "var(--max-width-custom-41x)",
         '100vw': "var(--max-width-100vw)"
       },
       inset: {
-        '1px': 'var(--inset-1px)',
+        // TODO: change 1px to px in the components
+        // '1px': 'var(--inset-1px)',
         'header-height': 'var(--inset-header-height)'
       },
       zIndex: {
-        '1': 'var(--z-index-1)',
+        1: 'var(--z-index-1)',
         2000: 'var(--z-index-2000)'
       },
       backdropSaturate: {
         180: 'var(--backdrop-saturate-180)',
       },
-      willChange: {
-        'height': 'var(--will-change-height)',
-      },
-      willChange: {
-        'height': 'var(--will-change-height)',
-        164: "var(--max-width-164)",
-        '100vw': "var(--max-width-100vw)"
-      },
-      inset: {
-        '1px': 'var(--inset-1px)',
-        'header-height': 'var(--inset-header-height)'
-      },
-      zIndex: {
-        '1': 'var(--z-index-1)',
-        2000: 'var(--z-index-2000)'
-      },
       backdropSaturate: {
         180: 'var(--backdrop-saturate-180)',
-      },
-      willChange: {
-        'height': 'var(--will-change-height)',
-      },
-      willChange: {
-        'height': 'var(--will-change-height)',
       },
       animation: {
         'loading-blink': 'loading-blink 1.4s infinite both',
@@ -423,13 +288,6 @@ module.exports = {
         "accordion-up": "accordion-up 0.2s ease-out",
         "fade-in": "fade-in 0.2s ease",
         "fade-out": "fade-out 0.2s ease",
-        'loading-blink': 'loading-blink 1.4s infinite both',
-        'navigation-menu-scale-in': 'navigation-menu-scale-in 0.2s ease',
-        'navigation-menu-scale-out': 'navigation-menu-scale-out 0.2s ease',
-        "accordion-down": "accordion-down 0.2s ease-out",
-        "accordion-up": "accordion-up 0.2s ease-out",
-        "fade-in": "fade-in 0.2s ease",
-        "fade-out": "fade-out 0.2s ease"
       },
       keyframes: {
         'loading-blink': {
@@ -460,42 +318,18 @@ module.exports = {
           '0%': { opacity: 1 },
           '100%': { opacity: 0 },
         },
-        },
-        'navigation-menu-scale-in': {
-          '0%': { opacity: 0, transform: 'rotateX(-30deg) scale(.9)' },
-          '100%': { opacity: 1, transform: 'rotateX(0deg) scale(1)'},
-        },
-        'navigation-menu-scale-out': {
-          '0%': { opacity: 1, transform: 'rotateX(0deg) scale(1)'},
-          '100%': { opacity: 0, transform: 'rotateX(-30deg) scale(.9)' },
-        },
-        "accordion-down": {
-          from: { height: 0 },
-          to: { height: "var(--radix-accordion-content-height)" },
-        },
-        "accordion-up": {
-          from: { height: "var(--radix-accordion-content-height)" },
-          to: { height: 0 },
-        "fade-in": {
-          '0%': { opacity: 0 },
-          '100%': { opacity: 1 },
-        },
-        "fade-out": {
-          '0%': { opacity: 1 },
-          '100%': { opacity: 0 },
-        },
         }
       }
     },
+    plugins: [
+      require('tailwindcss-animate'),
+      plugin(({ matchUtilities }) => {
+        matchUtilities({
+          perspective: (value) => ({
+            perspective: value,
+          }),
+        });
+      }),
+    ],
   },
-  plugins: [
-    require('tailwindcss-animate'),
-    plugin(({ matchUtilities }) => {
-      matchUtilities({
-        perspective: (value) => ({
-          perspective: value,
-        }),
-      });
-    }),
-  ],
-};
+}
